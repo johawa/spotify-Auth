6a3ab92410b876bc59a3ebf06b1272a3b1860acd
Johannes Walenta <johannes.walenta@googlemail.com>
2018-10-02 14:21:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.get("/callback", function(req, res) {
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token;
     let uri = process.env.FRONTEND_URI || "http://localhost:3000";
-    res.redirect(uri + "?access_token=" + access_token);
     localStorage.setItem('TOKEN', `${token}`);
+    res.redirect(uri + "?access_token=" + access_token);   
   });
 });
 
